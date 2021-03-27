5ee52543c9c1edb445452fba72a990fd363cb15a
Sean Mahoney <68126629+technosheen@users.noreply.github.com>
2021-03-27 13:08:59 -0400

    Initialize add-example-ui

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      default: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
