d3aa1640712755bff5178d4da37e292dfe4546d0
Sean Mahoney <68126629+technosheen@users.noreply.github.com>
2021-03-27 13:09:02 -0400

    Initialize genarate_with_custom_logo

---
+++ app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Logo" src="../assets/images/logo.png" width="350">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  display: block;
+  margin: 15px   auto;
+}
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
